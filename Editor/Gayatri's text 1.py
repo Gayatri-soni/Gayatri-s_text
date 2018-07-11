ebd4d2205dd01799d325b06383793b5f51d7590d
gayatri_soni <gayatrisoni5496@gmail.com>
2018-07-11 21:04:54 +0530

    version 2 with a toolbar of my text editor

--- Editor/Gayatri's text 1.py
+++ Editor/Gayatri's text 1.py
@@ -19,24 +19,27 @@ import inspect
 import importlib
 
 
-
+file = None
 def Quit():
     def opn():
 
         text.delete(1.0, END)
 
+        global file
         file = open(askopenfilename(filetypes=(("Template files", "*.tplate"),
                                            ("HTML files", "*.html;*.htm"),
-                                           ("All files", "*.*"),("Text files","*.txt"),("Python files","*.py") )), 'r')
+                                           ("All files", "*.*"),("Text files","*.txt"),("Python files","*.py") )))
 
         if file != '':
-            v=str(file)
-            v.split()
+            x = str(file)
+            y = x.split()[1]
+            z = y.split("\'")[1]
+            v=z.split("\\")[-1]
             t.title(os.path.basename(v)+"- Gayatri's text")
             txt = file.read()
 
             text.insert(INSERT, txt)
-
+            file.close()
         else:
 
             pass
@@ -55,13 +58,32 @@ def Quit():
         if filename:
             alltext = text.get(1.0, END)
 
-            open(filename, 'w').write(alltext)
+            f=open(filename, 'w')
+            f.write(alltext)
+            x = str(f)
+            # print(x)
+            # print(f)
+            y = x.split()[1]
+            z = y.split("/")[-1]
+            # print("value of z=",z)
+            # v = z.split("\\")
+            # print("val of v=",v)
+            # c=v.split("\\")
+            # print("Value of c: ",c)
+
+            t.title(z+"- Gayatri's text")
 
 
 
 
     def show():
-        showinfo("Gayatri's text","author and developer-Gayatri(the decider)")
+        ab=Toplevel(t)
+        ab.iconbitmap(r'C:\Users\user\Downloads\letter-g-icon-png-21704-Windows.ico')
+        ab.title("Information")
+        l=Label(ab,text="Gayatri's text\nauthor and developer-Gayatri(the decider)",fg="cyan",bg="grey",font=("Elephant",20))
+        ab.geometry("580x90")
+        l.pack()
+        # showinfo("Gayatri's text","author and developer-Gayatri(the decider)")
 
     def normal():
         text.config(font=("Arial", 20))
@@ -93,6 +115,22 @@ def Quit():
         if color:
             text.config(background=color)
 
+    def delete():
+        MsgBox = messagebox.askquestion('Delete File', 'Are you sure you want to delete the file?',icon='warning')
+        if MsgBox == 'yes':
+            x = str(file)
+            y = x.split()[1]
+            t.destroy()
+
+            y.split("\'")[1]
+            os.remove(y.split("\'")[1])
+            Quit()
+            # os.remove(os.path.curdir)
+
+        else:
+            messagebox.showinfo('Return', 'You will now return to the application screen')
+
+
 
 
 
@@ -106,6 +144,55 @@ def Quit():
     t.grid_rowconfigure(0,weight=1)
     t.grid_columnconfigure(0,weight=1)
     t.geometry('600x600')
+    toolbaar = Frame(t, bd=2, relief=RAISED)
+
+    img = Image.open(r"C:\Users\user\Desktop\new.jpg")
+    photo1 = ImageTk.PhotoImage(img)
+    new = Button(toolbaar, bd=5,bg="grey", width=22,height=22, relief=FLAT,image=photo1,command=new_file)
+    new.pack(side=LEFT, padx=2, pady=2)
+
+    img = Image.open(r"C:\Users\user\Desktop\open.jpg")
+    photo2 = ImageTk.PhotoImage(img)
+    open1=Button(toolbaar, bd=5,bg="grey", width=22,height=22, relief=FLAT,image=photo2,command=opn)
+    open1.pack(side=LEFT, padx=2, pady=2)
+
+    img = Image.open(r"C:\Users\user\Desktop\cut.jpg")
+    photo3 = ImageTk.PhotoImage(img)
+    cut=Button(toolbaar, bd=5,bg="grey", width=22,height=22, relief=FLAT,image=photo3,command=lambda :t.focus_get().event_generate("<<Cut>>"))
+    cut.pack(side=LEFT, padx=2, pady=2)
+
+    img = Image.open(r"C:\Users\user\Desktop\copy.png")
+    photo4 = ImageTk.PhotoImage(img)
+    copy = Button(toolbaar, bd=5, bg="grey", width=22, height=22, relief=FLAT, image=photo4,command=lambda: t.focus_get().event_generate("<<Copy>>"))
+    copy.pack(side=LEFT, padx=2, pady=2)
+
+    img = Image.open(r"C:\Users\user\Desktop\paste.jpg")
+    photo5 = ImageTk.PhotoImage(img)
+    paste = Button(toolbaar, bd=5, bg="grey", width=22, height=22, relief=FLAT, image=photo5,command=lambda: t.focus_get().event_generate("<<Paste>>"))
+    paste.pack(side=LEFT, padx=2, pady=2)
+
+    img = Image.open(r"C:\Users\user\Desktop\find.jpg")
+    photo6 = ImageTk.PhotoImage(img)
+    find = Button(toolbaar, bd=5, bg="grey", width=22, height=22, relief=FLAT, image=photo6)
+    find.pack(side=LEFT, padx=2, pady=2)
+
+    img = Image.open(r"C:\Users\user\Desktop\info.png")
+    photo7 = ImageTk.PhotoImage(img)
+    info1 = Button(toolbaar, bd=5, bg="grey", width=22, height=22, relief=FLAT, image=photo7)
+    info1.pack(side=LEFT, padx=2, pady=2)
+
+    img = Image.open(r"C:\Users\user\Desktop\undo.png")
+    photo8 = ImageTk.PhotoImage(img)
+    undo = Button(toolbaar, bd=5, bg="grey", width=22, height=22, relief=FLAT, image=photo8,command=lambda:t.focus_get().event_generate("<<Undo>>"))
+    undo.pack(side=LEFT, padx=2, pady=2)
+
+    img = Image.open(r"C:\Users\user\Desktop\redo.jpg")
+    photo9 = ImageTk.PhotoImage(img)
+    redo = Button(toolbaar, bd=5, bg="grey", width=22, height=22, relief=FLAT, image=photo9,command=lambda:t.focus_get().event_generate("<<Redo>>"))
+    redo.pack(side=LEFT, padx=2, pady=2)
+
+
+    toolbaar.pack(side=TOP, fill=X)
 
     text = Text(t,  font=("Arial", 20))
 
@@ -113,7 +200,7 @@ def Quit():
 
     scroll.config(command=text.yview)
 
-    text.config(yscrollcommand=scroll.set,bg="PeachPuff")
+    text.config(yscrollcommand=scroll.set,bg="white")
 
     scroll.pack(side=RIGHT, fill=Y)
 
@@ -140,7 +227,7 @@ def Quit():
     editmenu.add_command(label='Cut             ',accelerator='Ctrl+X',command=lambda :t.focus_get().event_generate("<<Cut>>"))
     editmenu.add_command(label='Copy            ',accelerator='Ctrl+C',command=lambda :t.focus_get().event_generate("<<Copy>>"))
     editmenu.add_command(label='Paste           ',accelerator='Ctrl+V',command=lambda :t.focus_get().event_generate("<<Paste>>"))
-    editmenu.add_command(label='Delete'          ,accelerator='DEL',command=lambda:t.focus_get().event_generate(showwarning("do you really want to delete")))
+    editmenu.add_command(label='Delete'          ,accelerator='Ctrl+Alt+D',command=delete)
     editmenu.add_command(label='Undo'            ,accelerator='Ctrl+Z',command=lambda:t.focus_get().event_generate("<<Undo>>"))
     editmenu.add_command(label='Redo'           ,accelerator='Ctrl+Y',command=lambda :t.focus_get().event_generate("<<Redo>>"))
 
@@ -178,7 +265,8 @@ def Quit():
 
 
 
-
+def abc():
+    print("Hello World!")
 
 
 root=Tk()
@@ -195,17 +283,20 @@ root.iconbitmap(r'C:\Users\user\Downloads\letter-g-icon-png-21704-Windows.ico')
 im=Image.open(r"C:\Users\user\Desktop\gayu.jpg")
 photo=ImageTk.PhotoImage(im)
 
-cv = Canvas()
-cv.pack( fill='both', expand=True)
+# cv = Canvas()
+# cv.config(width=600,height=600)
+# cv.place( x=0,y=0,relwidth=1,relheight=1)
 
 
-cv.create_image(50, 50, image=photo, anchor='nw')
+# cv.create_image(50, 50, image=photo)
+l=Label(image=photo)
+l.place(x=0,y=0,relwidth=1,relheight=1)
 
-b=Button(cv,justify=LEFT,bd=5,width=60,bg="grey",command=Quit)
+b=Button(l,justify=LEFT,bd=5,width=60,bg="grey",command=Quit)
 img=Image.open(r"C:\Users\user\Desktop\jf.jpg")
 photo2=ImageTk.PhotoImage(img)
 b.config(image=photo2)
-cv.create_window(10,10,anchor="s",window=b)
+# cv.create_window(200,200,anchor="center",window=b)
 b.pack()
 
 
